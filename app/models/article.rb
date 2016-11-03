97de3ee45214f47990391c1a2ec4f3d5090f43eb
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2016-11-03 11:16:53 +0100

    Comments generated

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ActiveRecord::Base
    belongs_to :user
 
    validates :title, presence: true,
-             lenght: { maximum: 50 }
+             length: { maximum: 50 }
    validates :post, presence: true
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
 
    has_many :articles
 
-   validates :password, presence: true
+   validates :password, presence: true,
+              length: { minimum: 8 }
    validates :email, presence: true
 
 end
